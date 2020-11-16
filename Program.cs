1acf7eaceacf9a3bf6625e84081efa0e65406e78
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-15 23:13:07 -0300

    Desafio adicionado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade>=18){
+                Console.WriteLine("Você pertence a categoria Sênior");
+            }else if(idade>=14 && idade<=17){
+                Console.WriteLine("Você pertence a categoria Juvenil B");    
+            }else if(idade>=11 && idade<=13){
+                Console.WriteLine("Você pertence a categoria Juvenil A");
+            }else if(idade>=8 && idade<=10){
+                Console.WriteLine("Você pertence a categoria Infantil B");
+            }else if(idade>=5 && idade<=7){
+                Console.WriteLine("Você pertence a categoria Infantil A");
+            }else{
+                Console.WriteLine("Você não tem idade suficiente");
+            }
+
+        }//FIM DO MAIN
+    }
+}
